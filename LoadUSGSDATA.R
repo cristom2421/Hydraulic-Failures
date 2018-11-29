af5522bcb89e815404f81b2d9294dfc1cac7e004
Carlos Rubio <carlos@Carloss-MacBook-Pro.local>
2018-11-29 13:55:08 -0700

    changes again

--- LoadUSGSDATA.R
+++ LoadUSGSDATA.R
@@ -9,8 +9,10 @@ SitesChar <- c("13015000", "03196800", "03155000", "03117500", "01611500", "0161
 
 #pcode <- "62856"
 
+
+
 data <- df.Fail.NBI.Gage[]
-head(data$STAID)
+head(data$YR_FAIL_EST)
 
 rows <- c(1,2,3,4,5,6)
 BeginDate <- sapply(rows, function(i) max(as.Date(as.numeric(df.Fail.NBI.Gage[i,"DATE_P_BEGIN_ALT_USGS"]),"1970-01-01"),df.Fail.NBI.Gage[i,"YR_BLT_EST"],na.rm = TRUE))
@@ -32,16 +34,24 @@ EndDate   <- paste(EndDate,"12","31",sep="-")
 ls.Discharge <- vector("list",length=length(SitesChar))
 
 for (i in 1:length(SitesChar)){
-  dat <- importDVs(SitesChar[i],code="00060",stat="00003",sdate=BeginDate[i],edate=EndDate[i])
+  dat <- readNWISdata(SitesChar[i],code="00060",stat="00003",sdate=BeginDate[i],edate=EndDate[i])
 
   ls.Discharge[[i]]$staid <- dat$staid
   ls.Discharge[[i]]$val   <- dat$val
   ls.Discharge[[i]]$dates <- dat$dates
 }
 
-BeginDate
-EndDate
+HasFailDate <- !is.na(df.Fail.NBI.Gage[rows,"YR_FAIL"])
+NoFailDate  <- !HasFailDate
+FailDate    <- character(length(rows))
+FailDate[HasFailDate] <- df.Fail.NBI.Gage[rows[HasFailDate],"YR_FAIL"]
 
+NoFailIndex  <- c(1:length(rows))[NoFailDate]
+Dates <- lapply(ls.Discharge,"[[",3)
+BeginFailYearIndex <- sapply(NoFailIndex, function(i) which.min(abs(Dates[[SitesChar[i]]]-df.Fail.NBI.Gage[rows[i],"YR_FAIL"])))
+EndFailYearIndex <- sapply(NoFailIndex, function(i) which.min(abs(Dates[[SitesChar[i]]]-as.Date(paste(substr(df.Fail.NBI.Gage[rows[i],"YR_FAIL"],1,4),"12","31",sep="-")))))
+FailDate[NoFailDate] <- sapply(1:length(BeginFailYearIndex), function(i) ls.Discharge[[NoFailIndex[i]]][which.max(ls.Discharge[[NoFailIndex[i]]]$val[BeginFailYearIndex[i]:EndFailYearIndex[i]])+BeginFailYearIndex[i],"dates"])
+FailDate <- as.Date(as.numeric(FailDate),"1970-01-01")
 
 #head(data$DATE_P_BEGIN_ALT_USGS)
 #head(data$YR_BLT_EST)
