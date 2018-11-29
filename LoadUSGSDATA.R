fa613809ba7b74dd35fb99004d1686e5f1a41094
Carlos Rubio <carlos@Carloss-MacBook-Pro.local>
2018-11-29 12:24:57 -0700

    fixing -inf

--- LoadUSGSDATA.R
+++ LoadUSGSDATA.R
@@ -1,6 +1,5 @@
 #LoadUSGS Peak FLow Data
 #November 13, 2018
-
 library(dataRetrieval)
 
 
@@ -12,6 +11,9 @@ data <- df.Fail.NBI.Gage[]
 
 rowsforanalysis <- c(1,2,3,4,5,6)
 BeginDate <- sapply(rowsforanalysis, function(i) max(as.Date(as.numeric(df.Fail.NBI.Gage[i,"DATE_P_BEGIN_ALT_USGS"]),"1970-01-01"),df.Fail.NBI.Gage[i,"YR_BLT_EST"],na.rm = TRUE))
+
+BeginDate <- BeginDate[BeginDate != "-Inf"]
+
 BeginDate <- as.Date(BeginDate,"%Y-%m-%d",origin="1970-01-01")
 BeginDate <- sapply(BeginDate, function(y) substr(y,1,4))
 BeginDate <- paste(BeginDate,"01","01",sep="-")
--- Work in progress LoadFFA.R
+++ Work in progress LoadFFA.R
@@ -1,50 +1,41 @@
 # Function to load flood frequency analysis results from output file
 # Written by Madeleine Flint, 2018-06-27
 # Supports HEC-SSP v2? Bulletin 17B analysis, PeakFQ v4.2 B17B, and PeakFQ v7.2 B17C
-LoadFFA <- function(filename, folder.out, TYPE = "PeakFQ_17C",
-                    colStandard = TRUE){ # standardize column output names
-
-  ls.colNames <- list("PEAKFQSA" = c("Ann. Exc. Prob.", "EMA Est.", "V[log(EMA)]","At-Site Est.","CI-Low","CI-High"),
-                      "PKFQ72_17C" = c("ANN_EXC_PROB",  "EMA_REG",	"EMA_STAT",	"LOG_VAR_REG",	"5PCT_CONF_REG",	"95PCT_CONF_REG"),
-                      "HEC_17B"    = c("Ordinate",  "FREQ",	"FLOW Computed Curve",	"FLOW Expected Prob Curve",	"FLOW 5Pct Conf",	"FLOW 95Pct Conf",
-                                       "FLOW Observed (Median)",	"FLOW Historic Data",	"FLOW High Outlier",	"FLOW Low Outlier"),
-                      "PKFQ42_17B" = c("ANN_EXC_PROB",  "BULL17B_EST",	"SYSTEM_RECORD",	"EXPECTED_PROB",	"5PCT_CONF",	"95PCT_CONF"),
-                      "PARTDUR"    = c("Val","Exc"))
+#setwd("~/Documents/cris")
+LoadFFA <- function(filename, folder.out, TYPE = "PeakFQ_17C", colStandard = TRUE){ 
+  
+  # standardize column output names
+  ls.colNames <- list("PEAKFQSA" = c("Ann. Exc. Prob.", "EMA Est.", "V[log(EMA)]","At-Site Est.","CI-Low","CI-High"), "PKFQ72_17C" = c("ANN_EXC_PROB",  "EMA_REG",	"EMA_STAT",	"LOG_VAR_REG",	"5PCT_CONF_REG",	"95PCT_CONF_REG"), "HEC_17B"    = c("Ordinate",  "FREQ",	"FLOW Computed Curve",	"FLOW Expected Prob Curve",	"FLOW 5Pct Conf",	"FLOW 95Pct Conf", "FLOW Observed (Median)",	"FLOW Historic Data",	"FLOW High Outlier",	"FLOW Low Outlier"), "PKFQ42_17B" = c("ANN_EXC_PROB",  "BULL17B_EST",	"SYSTEM_RECORD",	"EXPECTED_PROB",	"5PCT_CONF",	"95PCT_CONF"), "PARTDUR"    = c("Val","Exc"))
   colNames <- ls.colNames[TYPE]
 
-  colsSwitch <- list(FlowCols   = c("EMA_REG", "FLOW Computed Curve", "BULL17B_EST","Val","EMA Est.","V[log(EMA)]","At-Site Est."),
-                     FreqCols   = c("Ann. Exc. Prob.","ANN_EXC_PROB","FREQ","Exc","Ann. Exc. Prob."),
-                     Flow05Cols = c("5PCT_CONF_REG", "5PCT_CONF","CI-Low"),
-                     Flow95Cols = c("95PCT_CONF_REG", "95PCT_CONF","CI-High"))
+  colsSwitch <- list(FlowCols   = c("EMA_REG", "FLOW Computed Curve", "BULL17B_EST","Val","EMA Est.","V[log(EMA)]","At-Site Est."), FreqCols   = c("Ann. Exc. Prob.","ANN_EXC_PROB","FREQ","Exc","Ann. Exc. Prob."), Flow05Cols = c("5PCT_CONF_REG", "5PCT_CONF","CI-Low"), Flow95Cols = c("95PCT_CONF_REG", "95PCT_CONF","CI-High"))
 
-  colsOut <- c(FlowCols   = "Flow",
-               FreqCols   = "Freq",
-               Flow05Cols = c("Flow05"),
-               Flow95Cols = c("Flow95"))
+  colsOut <- c(FlowCols   = "Flow", FreqCols   = "Freq", Flow05Cols = c("Flow05"), Flow95Cols = c("Flow95"))
 
-fileinput<-file.path(folder.out,filename)
-fileinput<-c(fileinput)
+  fileinput<-file.path(folder.out,filename)
+  fileinput<-c(fileinput)
 
-require(data.table)
-if(grepl("PEAKFQSA",TYPE)){ # PeakfqSA Bulleting 17C analysis
-  text.list<-lapply(fileinput,readLines)
-  skip.rows<-sapply(text.list, grep, pattern = '^Ann. Exc. Prob.\\s+EMA Est.')-1
-  PFA <- lapply(seq_along(text.list),function(i) read.delim(fileinput[i],skip=skip.rows[i],sep="\n",stringsAsFactors = TRUE,blank.lines.skip = FALSE))
-}
-#PFA<-as.data.frame(PFA)
-for ( val in PFA ){
-  if ( val != ''){
-    print(val)
+  require(data.table)
+  if(grepl("PEAKFQSA",TYPE)){ # PeakfqSA Bulleting 17C analysis
+    text.list<-lapply(fileinput,readLines)
+    skip.rows<-sapply(text.list, grep, pattern = '^Ann. Exc. Prob.\\s+EMA Est.')-1
+    PFA <- lapply(seq_along(text.list),function(i) read.delim(fileinput[i],skip=skip.rows[i],sep="\n",stringsAsFactors = TRUE,blank.lines.skip = FALSE))
   }
-  else{
-    break
+  #PFA<-as.data.frame(PFA)
+  for ( val in PFA ){
+    if ( val != ''){
+      print(val)
+    }
+    else{
+      break
+    }
   }
+  #print(PFA)
+  class(PFA)
+  #strsplit(PFA," ")[[1]][1]
+  #print(PFA)
 }
-#print(PFA)
-class(PFA)
-#strsplit(PFA," ")[[1]][1]
-#print(PFA)
-}
+LoadFFA("03606500peaks.out","~/Documents/cris/","PEAKFQSA")
 #
 # if(grepl("PeakFQ72_17C",TYPE)){ # 7.2 version, non-batch
 #   PFA <- read.fwf(file.path(folder.out,filename),
